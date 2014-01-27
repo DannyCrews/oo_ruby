3890ebd7f02cadba83aee35894cbf62736433d71
Dan Crews <dancrews@me.com>
2014-01-26 19:09:51 -0500

    RevealingMethods class to demonstrate the separation of data and structure

--- obscuring_references.rb
+++ obscuring_references.rb
@@ -12,3 +12,25 @@
 #   end
 #   #... many other methods that index into the array
 # end
+
+# the diameters method below has no knowledge of the internal array structure
+class RevealingReferences
+  attr_reader :wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect {|wheel|
+      wheel.rim + (wheel.tire * 2)}
+  end
+
+  # instead of defining a class, Struct creates small, lightweight objects rim and tire
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect {|cell|
+      Wheel.new(cell[0], cell[1])}
+  end
+
+end
